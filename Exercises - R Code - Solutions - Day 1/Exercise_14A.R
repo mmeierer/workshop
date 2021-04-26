7517eb6a7aedd58fe03a81025414e0c2603444fa
Margot Loewenberg <margot.loewenberg@uzh.ch>
2021-04-26 16:06:12 +0200

    Solution ex 14A

---
+++ Exercises - R Code - Solutions - Day 1/Exercise_14A.R
@@ -0,0 +1,19 @@
+# 14A: Preparations ####
+
+# 1. Set your working directory ####
+setwd("")
+
+# 2. Install (if necessary) and load the following packages. ####
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(Hmisc)
+
+# 3. Read in the data transactions.csv and name it transactions. ####
+transactions <- fread("transactions.csv")
+
+# Check the data types
+str(transactions)
+
+# 4. Transform the variable TransDate. ####
+transactions[, TransDate:=dmy(TransDate)]
